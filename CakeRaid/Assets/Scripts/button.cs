b7bd4525c3669d0b53d07e4fd9a4c8be9a9e6de0
Josh Davis <dtendr@gmail.com>
2014-12-25 05:00:21 -0500

    Added game script
    Started on cursor change

--- CakeRaid/Assets/Scripts/button.cs
+++ CakeRaid/Assets/Scripts/button.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class buttonBehavior : MonoBehaviour {
+public class button : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ CakeRaid/Assets/Scripts/game.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class game : MonoBehaviour {
+	//GameObject cursor;
+	// Use this for initialization
+	void Start () {
+		//cursor = GameObject.FindWithTag("cursor");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	Texture2D cursorTexture = Resources.Load<Texture2D>("Cursor");
+	CursorMode cursorMode = CursorMode.Auto;
+	Vector2 hotSpot = Vector2.zero;
+	void OnMouseEnter () {
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+	void OnMouseExit () {
+		Cursor.SetCursor(null, Vector2.zero, cursorMode);
+	}
+}
