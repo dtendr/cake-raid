2f8b209b0b1d4b6d55158c5d14eef0863ef49c42
Josh Davis <dtendr@gmail.com>
2014-12-27 21:08:48 -0500

    Added Trap type classes
    Also fixed their namespaces

--- CakeRaid/Assets/Scripts/Traps/TrapZapper.cs
+++ CakeRaid/Assets/Scripts/Traps/TrapZapper.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrapZapper : MonoBehaviour {
+namespace CakeRaid {
+	public class TrapZapper : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
 	}
 }
