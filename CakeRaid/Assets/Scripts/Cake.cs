56912438f64823cb6a7a4b8ae74ee6923baad7ce
Josh Davis <dtendr@gmail.com>
2015-07-29 11:52:46 -0400

    Removed old cursor stuff, added play icon

--- CakeRaid/Assets/Scripts/Cake.cs
+++ CakeRaid/Assets/Scripts/Cake.cs
@@ -3,32 +3,15 @@ using System.Collections;
 
 namespace CakeRaid{
 	public class Cake : MonoBehaviour {
-		//GameObject cursor;
-
-		public Texture2D cursorTexture;
-		public CursorMode cursorMode;
-		public Vector2 hotSpot;
 
 		// Use this for initialization
 		void Start () {
-			cursorTexture = Resources.Load<Texture2D>("Textures/Cursor");
-			cursorMode = CursorMode.Auto;
-			hotSpot = Vector2.zero;
-
 
 		}
 		
 		// Update is called once per frame
 		void Update () {
 			
-		}
-			
-		void OnMouseEnter () {
-			Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-		}
-
-		void OnMouseExit () {
-			Cursor.SetCursor(null, Vector2.zero, cursorMode);
 		}
 	}
 }
